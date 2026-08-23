f681d1bc520dc7b2148024f1b231dd24bcbd7492
Mrshubham21 <st689801@gmail.com>
2026-08-23 14:28:46 +0530

    change

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -5,7 +5,7 @@ const generateToken = (userId) => {
     { id: userId },
     process.env.JWT_SECRET,
     {
-      expiresIn: "20d",
+      expiresIn: process.env.JWT_EXPIRES_IN || "20d",
     }
   );
 };
