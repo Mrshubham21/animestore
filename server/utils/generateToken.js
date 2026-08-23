a7361052526e848cab9c2cf5c7466eef9482c06d
Mrshubham21 <st689801@gmail.com>
2026-08-23 14:24:22 +0530

    change

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
-console.log("JWT_EXPIRES_IN:", process.env.JWT_EXPIRES_IN);
+
 const generateToken = (userId) => {
   return jwt.sign(
     { id: userId },
     process.env.JWT_SECRET,
     {
-      expiresIn: process.env.JWT_EXPIRES_IN,
+      expiresIn: "20d",
     }
   );
 };
